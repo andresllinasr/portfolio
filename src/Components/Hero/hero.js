f33d7cc4d1eb467358792f0842a0af8c73503f24
andresllinasr <andresllinasr@gmail.com>
2024-03-23 19:23:11 -0500

    Update global styles and hero component

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import './hero.scss';
-import designer from '../../assets/designer.svg'
+import avatar from '../../assets/avatar.svg'
+import swipeIcon from '../../assets/swipe-icon.svg'
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="hero-content">
-        <h1>WIP: Mixing Colors, Lines, and Pixels</h1>
-        <div className="img-wrapper">
-            <img src={designer} alt="Designer" />
+        <div className="hero-left">
+          <h1 className='hero-title'>Hi, My name is<br /><span className="highlighted-text">Andrés Llinás</span></h1>
+          <p className="hero-header">I develop exceptional and accessible digital experiences for the web.</p>
+          <div className="hero-ctas cta-group">
+            <a className='hero-cta primary-cta' href="#">Download CV</a>
+            <a className='hero-cta secondary-cta' href="#">Get in touch</a>
+          </div>
         </div>
-        <p className='subheader'> By <a href='https://www.linkedin.com/in/andresllinasr'>Andrés Llinás</a></p>
-
-        <svg className="blob blob-1" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+        <div className="hero-right">
+          <svg className="blob blob-1" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
             <defs>
-                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" stop-color="#F24405" stop-opacity="1" />
-                    <stop offset="100%" stop-color="#655D5D" stop-opacity="1" />
-                </linearGradient>
+              <linearGradient id="gradient" x1="0%" y1="100%" x2="0%" y2="0%">
+                <stop offset="0%" stop-color="#000" stop-opacity=".7" />
+                <stop offset="100%" stop-color="#000" stop-opacity="0" />
+              </linearGradient>
             </defs><path d="M220,412.89352842548396C261.930381224514,405.25996496708245,281.9646644615737,360.0360803985666,316.16815118907886,334.60873967272784C354.10973465025916,306.40244661169146,419.3871857911462,303.1135039322799,429.72599323760676,256.9803511636712C440.01532735458125,211.06795506153225,384.6178465106194,179.26669776611024,359.962458222292,139.19263706891803C336.41454919700305,100.91863689759859,328.6011098108377,51.200232356148206,289.49741710550364,29.057415792209305C246.64647856418782,4.7926876791135875,188.55697181145314,-7.424447969301998,146.6179142382798,18.384376377191135C105.78773944393676,43.51080673307661,119.33204387172324,106.52327445246692,95.76942434103401,148.27544370171375C72.64752467502184,189.2466721749346,13.521378675953256,209.6232601624374,12.154659238994146,256.64874139071014C10.737011700115271,305.4265343488495,50.14660381935017,346.61528213728025,89.152791660316,375.93763061974136C126.50651514639426,404.01776246041186,174.02459348004524,421.2635020089986,220,412.89352842548396" fill="url(#gradient)" />
-        </svg>
-        <svg className="blob blob-2" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" stop-color="#F24405" stop-opacity="1" />
-                    <stop offset="100%" stop-color="#F8FD63" stop-opacity="1" />
-                </linearGradient>
-            </defs><path d="M220,415.15973614352146C275.1186086062856,414.88850554023327,255.9181172281912,318.7684777595463,285.65976981945613,272.3619191511603C307.9685293787845,237.55306800230312,366.2719416536099,227.90403635944267,366.50766745341315,186.56058095563628C366.7715843025978,140.27275631496263,327.1805943733792,103.93143578351301,286.7485951839993,81.39511390928025C242.71671761535313,56.8522620714105,191.6774534037977,45.63391037208848,144.2323985969528,62.66695451960828C90.00923966695653,82.1333765446781,22.239089897197665,117.88727491451841,24.781734432107683,175.44270480236523C27.281993231797408,232.03868905075183,117.75161820815175,229.08457997924123,153.52488457265267,273.0121355761956C188.27613286852298,315.6847105663074,164.96796769797922,415.43054071749157,220,415.15973614352146" fill="url(#gradient)" />
-        </svg>
+          </svg>
+          <img className='hero-image' src={avatar} alt="Designer" />
+        </div>
       </div>
+      <img className='swipe-icon' src={swipeIcon} alt="Swipe Down" />
     </section>
   );
 };
